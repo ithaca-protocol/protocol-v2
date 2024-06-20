577cbfebcbc68741c0fb8d25bed40563c45647e9
blockchainluffy <jainpunit1997@gmail.com>
2024-06-20 19:53:41 +0530

    feat: add test for liquidation when ithaca-collateral drops

--- test-suites/test-aave/ithaca.liquidation.e2e.spec.ts
+++ test-suites/test-aave/ithaca.liquidation.e2e.spec.ts
@@ -181,9 +181,6 @@ makeSuite('', (testEnv) => {
 
       const userGlobalDataAfter = await pool.getUserAccountData(borrower.address);
 
-      console.log(userGlobalDataBefore);
-      console.log(userGlobalDataAfter);
-
       expect(userGlobalDataAfter.totalDebtETH.toString()).to.be.bignumber.almostEqual(0);
       expect(userReserveDataAfter.currentVariableDebt.toString()).to.be.bignumber.eq(0);
     });
@@ -351,6 +348,160 @@ makeSuite('', (testEnv) => {
 
       expect(userGlobalDataAfter.totalDebtETH.toString()).to.be.bignumber.almostEqual(0);
       expect(userReserveDataAfter.currentVariableDebt.toString()).to.be.bignumber.eq(0);
+      expect(userGlobalDataAfter.availableBorrowsETH).to.be.eq(0);
+    });
+  });
+});
+
+makeSuite('', (testEnv) => {
+  const { INVALID_HF } = ProtocolErrors;
+
+  describe('reduce ithaca collateral and liquidate', () => {
+    let weth, users, pool, oracle, ithacaFeed: MockIthacaFeed, usdc, addressesProvider;
+    before('Before LendingPool liquidation: set config', async () => {
+      BigNumber.config({ DECIMAL_PLACES: 0, ROUNDING_MODE: BigNumber.ROUND_DOWN });
+      ({ weth, users, usdc, pool, oracle, ithacaFeed, addressesProvider } = testEnv);
+      await addressesProvider.setIthacaFeedOracle(ithacaFeed.address);
+    });
+
+    after('After LendingPool liquidation: reset config', () => {
+      BigNumber.config({ DECIMAL_PLACES: 20, ROUNDING_MODE: BigNumber.ROUND_HALF_UP });
+    });
+    it("It's not possible to liquidate on a non-active collateral or a non active principal", async () => {
+      const { configurator, weth, pool, users, usdc } = testEnv;
+      const user = users[1];
+      await configurator.deactivateReserve(usdc.address);
+
+      await expect(
+        pool.liquidationCall(weth.address, usdc.address, user.address, parseEther('1000'), false)
+      ).to.be.revertedWith('2');
+
+      await configurator.activateReserve(usdc.address);
+
+      await configurator.deactivateReserve(weth.address);
+
+      await expect(
+        pool.liquidationCall(weth.address, usdc.address, user.address, parseEther('1000'), false)
+      ).to.be.revertedWith('2');
+
+      await configurator.activateReserve(weth.address);
+    });
+
+    it('Deposits ithaca collateral, borrows weth', async () => {
+      const { usdc, weth, users, pool, oracle } = testEnv;
+      const depositor = users[0];
+      const borrower = users[1];
+
+      const fundlock = users[4];
+
+      //mints USDC to depositor
+      await weth
+        .connect(depositor.signer)
+        .mint(await convertToCurrencyDecimals(weth.address, '1000'));
+
+      //approve protocol to access depositor wallet
+      await weth.connect(depositor.signer).approve(pool.address, APPROVAL_AMOUNT_LENDING_POOL);
+
+      //depositor deposits 1000 USDC
+      const amountOfETHtoDeposit = await convertToCurrencyDecimals(weth.address, '2');
+
+      await pool
+        .connect(depositor.signer)
+        .deposit(weth.address, amountOfETHtoDeposit, depositor.address, '0');
+
+      await pool.connect(borrower.signer).setUsingIthacaCollateral(true);
+      const amountETHtoDeposit = await convertToCurrencyDecimals(weth.address, '2');
+
+      //mints weth to fundlock, a representation of 10usdc locked in the fundlock by the borrower.
+      await usdc
+        .connect(fundlock.signer)
+        .mint(await convertToCurrencyDecimals(usdc.address, '1000000'));
+
+      //approve protocol to access fundlock wallet
+      await usdc.connect(fundlock.signer).approve(pool.address, (100e18).toFixed(0));
+
+      await ithacaFeed.setData(
+        {
+          maintenanceMargin: 0,
+          mtm: 0,
+          collateral: amountETHtoDeposit,
+          valueAtRisk: 0,
+        },
+        1
+      );
+
+      const userGlobalData = await pool.getUserAccountData(borrower.address);
+
+      const amountWETHtoBorrow = new BigNumber(userGlobalData.availableBorrowsETH.toString())
+        .multipliedBy(0.8)
+        .toFixed(0);
+
+      await pool
+        .connect(borrower.signer)
+        .borrow(weth.address, amountWETHtoBorrow, RateMode.Variable, '0', borrower.address);
+      const userGlobalDataAfter = await pool.getUserAccountData(borrower.address);
+
+      expect(userGlobalDataAfter.currentLiquidationThreshold.toString()).to.be.bignumber.equal(
+        '10000',
+        INVALID_HF
+      );
+    });
+
+    it('Drop the health factor below 1', async () => {
+      const { usdc, users, pool, oracle } = testEnv;
+      const borrower = users[1];
+
+      await ithacaFeed.setData(
+        {
+          maintenanceMargin: 0,
+          mtm: 0,
+          collateral: 1e6,
+          valueAtRisk: 0,
+        },
+        1
+      );
+
+      const userGlobalData = await pool.getUserAccountData(borrower.address);
+
+      expect(userGlobalData.healthFactor.toString()).to.be.bignumber.lt(
+        oneEther.toFixed(0),
+        INVALID_HF
+      );
+    });
+
+    it('Liquidates the borrow', async () => {
+      const { usdc, weth, users, pool, helpersContract } = testEnv;
+      //   liquidator is fundlock
+      const fundlock = users[4];
+      const borrower = users[1];
+
+      const userGlobalDataBefore = await pool.getUserAccountData(borrower.address);
+      const amountToLiquidate = userGlobalDataBefore.totalDebtETH;
+
+      await increaseTime(100);
+
+      const fundlockBalBefore = await usdc.balanceOf(fundlock.address);
+
+      await pool
+        .connect(fundlock.signer)
+        .liquidateIthacaCollateral(
+          borrower.address,
+          amountToLiquidate,
+          usdc.address,
+          weth.address,
+          (1e6).toFixed(0),
+          { gasLimit: '80000000' }
+        );
+
+      const fundlockBalAfter = await usdc.balanceOf(fundlock.address);
+
+      const userGlobalDataAfter = await pool.getUserAccountData(borrower.address);
+
+      expect(fundlockBalAfter).to.be.equal(fundlockBalBefore.sub(1e6));
+
+      expect(userGlobalDataAfter.totalDebtETH.toString()).to.be.lt(
+        userGlobalDataBefore.totalDebtETH
+      );
       expect(userGlobalDataAfter.availableBorrowsETH).to.be.eq(0);
     });
   });
