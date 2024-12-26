b24c3230b7b82a256fcfed5f2370e82bc02ab4ee
chaitanyasjoshi <chaitanya9joshi@gmail.com>
2024-12-26 21:31:00 +0530

    fix: update rpc urls

--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -6,14 +6,14 @@ const GWEI = 1000 * 1000 * 1000;
 
 export const NETWORKS_RPC_URL: iParamsPerNetwork<string> = {
   [eEthereumNetwork.hardhat]: 'http://localhost:8545',
-  [eEthereumNetwork.arbitrumSepolia]: `https://arbitrum-sepolia.infura.io/v3/c351135c3bb54a779bf258ea5f1077d6`,
-  [eEthereumNetwork.arbitrum]: `https://arbitrum.llamarpc.com`,
+  [eEthereumNetwork.arbitrumSepolia]: `https://arb-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+  [eEthereumNetwork.arbitrum]: `https://arb-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
 };
 
 export const NETWORKS_DEFAULT_GAS: iParamsPerNetwork<number> = {
   [eEthereumNetwork.hardhat]: 65 * GWEI,
   [eEthereumNetwork.arbitrumSepolia]: 3 * GWEI,
-  [eEthereumNetwork.arbitrum]: 1 * GWEI,
+  [eEthereumNetwork.arbitrum]: 0.1 * GWEI,
 };
 
 export const BLOCK_TO_FORK: iParamsPerNetwork<number | undefined> = {
